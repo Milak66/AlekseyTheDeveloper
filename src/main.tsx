99ca9939c9fd11929a5c8cfa14389b73ca24db42
Aleksey <134363714+Milak66@users.noreply.github.com>
2025-06-12 15:44:22 +0300

    Aleksey's commit1.5

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
-import { Provider } from 'react-redux';
 import store from './components/store/store';
+import { Provider } from 'react-redux';
 
 const rootElement = document.getElementById('root') as HTMLDivElement; 
 if (rootElement) { 
