0b9d36ab18b044225c7cfaa3ef5735b4ad7eee27
Aleksey <134363714+Milak66@users.noreply.github.com>
2025-11-17 18:27:47 +0530

    Aleksey the developer 1.8.1

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -25,7 +25,7 @@ const Modal: React.FC<ModalProps> = () => {
     }
 
     try {
-      const response = await fetch("https://server-for-aleksey-the-developer.vercel.app/getData", {
+      const response = await fetch("https://aleksey-api-production.up.railway.app/getData", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
