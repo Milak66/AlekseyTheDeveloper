d48a2fc424110db1cd7c17c47760853f1308c6b0
Aleksey <134363714+Milak66@users.noreply.github.com>
2025-11-18 16:04:26 +0530

    Aleksey the developer 1.8.2 test1

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -25,7 +25,7 @@ const Modal: React.FC<ModalProps> = () => {
     }
 
     try {
-      const response = await fetch("https://aleksey-api.onrender.com/getData", {
+      const response = await fetch("https://aleksey-api-production.up.railway.app/getData", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
