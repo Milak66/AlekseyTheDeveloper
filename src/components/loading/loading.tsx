2dba8ea4801844aa8aa63c19c7b44af6d0f99492
Aleksey <134363714+Milak66@users.noreply.github.com>
2026-08-31 15:55:21 +0200

    Alexey the developer 1.2.4

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,30 +1,31 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from 'react';
 import './loading.css';
-import { useSelector, useDispatch } from "react-redux";
-import { RootState, AppDispatch } from "../store/store";
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '../store/store';
 import alexey from '../../assets/alexeyIcon.jpg';
-import { setLoading } from "../reduser/reduser";
+import { setLoading } from '../reduser/reduser';
 
-interface LoadingProps {};
+interface LoadingProps {}
 
 const Loading: React.FC<LoadingProps> = (): React.ReactNode => {
-  const [openWebsite, setOpenWebsite] = useState<Boolean>(false);
-  const textLang = useSelector((state: RootState) => state.aleksey.textLang); 
+  const [openWebsite, setOpenWebsite] = useState<boolean>(false);
+  const textLang = useSelector((state: RootState) => state.aleksey.textLang);
   const darkFunRef = useRef<HTMLImageElement>(null);
-  let pos = 1;
-  let posAround = 1;
-  let directionUp = true;
-
+  
   const dispatch = useDispatch<AppDispatch>();
 
   const stopLoading = () => {
     dispatch(setLoading(false));
-  }
+  };
 
   useEffect(() => {
     const dark_fun = darkFunRef.current;
-
     if (!dark_fun) return;
+
+    let pos = 1;
+    let posAround = 1;
+    let directionUp = true;
+    let animationFrameId: number;
 
     function loadingAnimation() {
       if (directionUp) {
@@ -43,26 +44,39 @@ const Loading: React.FC<LoadingProps> = (): React.ReactNode => {
         }
       }
       posAround += 3;
-      dark_fun!.style.transform = `rotate(${posAround}deg)`;
-      dark_fun!.style.bottom = pos + 'px';
-
-      requestAnimationFrame(loadingAnimation);
+      
+      if (dark_fun) {
+        dark_fun.style.transform = `rotate(${posAround}deg)`;
+        dark_fun.style.bottom = `${pos}px`;
+      }
+      
+      animationFrameId = requestAnimationFrame(loadingAnimation);
     }
 
     loadingAnimation();
 
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setOpenWebsite(true);
     }, 2000);
 
     return () => {
+      cancelAnimationFrame(animationFrameId);
+      clearTimeout(timer);
     };
   }, []);
 
   return (
     <div className="loading_place">
-      <img className="alexiLoading" ref={darkFunRef} src={alexey} alt="" />
-      {openWebsite ? <div className="loadingText" onClick={stopLoading}>{textLang.openWebsite}</div> : <div className="loadingText">{textLang.loadingText}</div>}
+      <img className="alexiLoading" ref={darkFunRef} src={alexey} alt="Loading icon" />
+      {openWebsite ? (
+        <div className="loadingText" onClick={stopLoading}>
+          {textLang.openWebsite}
+        </div>
+      ) : (
+        <div className="loadingText">
+          {textLang.loadingText}
+        </div>
+      )}
     </div>
   );
 };
